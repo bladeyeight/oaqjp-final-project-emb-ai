ddbc23eaca23a7c83a8124421b30e6e420471882
bladeyeight <samueljperry1991@gmail.com>
2024-02-15 14:53:10 -0500

    added error handling

--- EmotionDetection/emotion_detection.py
+++ EmotionDetection/emotion_detection.py
@@ -1,19 +1,31 @@
 import requests
 import json
 
+# def emotion_detector(text_to_analyse):
+#     url = 'https://sn-watson-emotion.labs.skills.network/v1/watson.runtime.nlp.v1/NlpService/EmotionPredict'
+#     myobj = { "raw_document": { "text": text_to_analyse } }
+#     header = {"grpc-metadata-mm-model-id": "emotion_aggregated-workflow_lang_en_stock"}
+#     response = requests.post(url, json = myobj, headers=header)
+#     return response.text
+
 def emotion_detector(text_to_analyse):
     url = 'https://sn-watson-emotion.labs.skills.network/v1/watson.runtime.nlp.v1/NlpService/EmotionPredict'
     myobj = { "raw_document": { "text": text_to_analyse } }
     header = {"grpc-metadata-mm-model-id": "emotion_aggregated-workflow_lang_en_stock"}
     response = requests.post(url, json = myobj, headers=header)
-    return response.text
-
-def emotion_predictor(text_to_analyse):
-    url = 'https://sn-watson-emotion.labs.skills.network/v1/watson.runtime.nlp.v1/NlpService/EmotionPredict'
-    myobj = { "raw_document": { "text": text_to_analyse } }
-    header = {"grpc-metadata-mm-model-id": "emotion_aggregated-workflow_lang_en_stock"}
-    response = requests.post(url, json = myobj, headers=header).json()
-    emotion_data = response['emotionPredictions'][0]['emotion']
+    if response.status_code == 400:
+        prediction_data = {
+        'anger': None,
+        'disgust': None,
+        'fear': None,
+        'joy': None,
+        'sadness': None,
+        'dominant_emotion': None
+        }
+        print('function', prediction_data)
+        return prediction_data 
+    responseJSON = response.json()
+    emotion_data = responseJSON['emotionPredictions'][0]['emotion']
     maxEmotion = max(emotion_data, key=emotion_data.get)
     prediction_data = {
         'anger': emotion_data['anger'],
--- server.py
+++ server.py
@@ -10,7 +10,9 @@ def home():
 @app.route('/emotionDetector')
 def detect_emotion():
     text_param = request.args.get('textToAnalyze')
-    response = EmotionDetection.emotion_detection.emotion_predictor(text_param)
+    response = EmotionDetection.emotion_detection.emotion_detector(text_param)
+    if response['dominant_emotion'] == None:
+        return "Invalid text!  Please try again!"
     return f"""For the given statement, the system response is 'anger': {response['anger']},
     'disgust': {response['disgust']}, 'fear': {response['fear']}, 'joy': {response['joy']},
     and 'sadness':{response['sadness']}, The dominant emotion is {response['dominant_emotion']}.""", 200
